77a59509b09ffcd06f3909522d756d18d55b31a8
Danilo Diniz <danilodiniz93@yahoo.com>
2024-01-14 17:55:46 -0300

    Start Project

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+    public class YourDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=./;Database=TransactionDB;Trusted_Connection=True;MultipleActiveResultSets=True;");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
